7ab87fe865b952ee057f62ec8d15ca578f54c5ec
Nick Golebiewski <nick.golebiewski@gmail.com>
2024-03-27 21:58:18 -0400

    try with comment out bcrypt

--- src/server/auth/register.js
+++ src/server/auth/register.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const prisma = require('../client');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 const saltRounds = 7;
 const jwt = require('jsonwebtoken');
 
